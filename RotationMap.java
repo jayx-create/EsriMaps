9e6daaf2c512cf0374213f027cb1246522a62332
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-07-05 21:42:17 -0700

    Update RotationMap.java

--- RotationMap.java
+++ RotationMap.java
@@ -15,6 +15,20 @@ public class RotationMap {
       ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
   }
 
-@Then("User points the compass North on ARCGIS")
+@Then("User verfies text on ARCGIS Map informing customer how to rotate map")
   public void userPointsTheCompassNorthOnARCGIS(){
-    
+
+    // creating a new map on ARCGIS Map
+    mapView = new MapView();
+      mapView.setMap(map);
+
+    // creating a compass for the map
+     int compass = new Compass(mapView);
+
+    // Creating and confirming the label
+      int instructionsLabel = new Label("Press the A and D keys to rotate the map.");
+      instructionsLabel.setTextFill(Color.RED);
+      int rotationLabel = new Label("Current rotation: ");
+      rotationLabel.setTextFill(Color.RED);
+  }
+}
