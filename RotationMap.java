e68a828668e45d124b4482134e9e55c02345d7ce
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-07-05 21:29:24 -0700

    Update RotationMap.java

--- RotationMap.java
+++ RotationMap.java
@@ -1,2 +1,16 @@
-When Customer authenticates the APY key for location 
-Then User point the compass north on ARCGIS 
+import io.cucumer.java.en.And;
+import io.cucumer.java.en.Given;
+import io.cucumer.java.en.Then;
+import io.cucumer.java.en.When;
+
+public class RotationMap {
+
+@When("Customer authenticates the API key for location")
+  public void customerAuthenticatesTheAPIKeyForLocation(){
+    String yourAPIKey = System.getProperty("apiKey");
+      ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
+  }
+
+@Then("User points the compass north on ARCGIS")
+  public void userPointsTheCompassNorthOnARCGIS(){
+    
