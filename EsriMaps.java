a318a192ebc7869b2e6b93c9dc7bee128b15aa41
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-07-05 21:20:22 -0700

    Update EsriMaps.java

--- EsriMaps.java
+++ EsriMaps.java
@@ -1,1 +1,42 @@
+import io.cucumer.java.en.And;
+import io.cucumer.java.en.Given;
+import io.cucumer.java.en.Then;
+import io.cucumer.java.en.When;
 
+public class LogInSteps {
+  
+  
+@Given("{string} is logged into Cal Poly Pomona portal")
+  public void loginProfileIdIsLoggedIn(String loginProfileId) throws Exception{
+   baseUtil.userInformation = TestUserUtil.getTestUserByProfileId(loginProfileId);
+   realData = baseutil.userInformation;
+   name = realData.getUserName();
+   switch(deviceMode.toUpperCase()) {
+     case GlobalConstant.DESKTOP:
+       if ( expectedLanguage.equals("English"){
+         cppPortal.invoke(username);
+         cppPortal.waitForPageToLoad();
+       }
+       else if( expectedLanguage.equals("Spanish"){
+         cppPortal.invoke(username);
+         cppPortal.waitForPageToLoad();
+   }
+ }           
+ @And ("User is registered with {string}")
+  public void userIsRegisteredWithLanguage(String language) throws Exception{
+  if(userLanguage. equalsIgnoreCase (GlobalConstant.ENGLISH)) {
+baseutil.deviceProp.setPreferredLanguage(GlobalConstant.ENGLISH);
+if(!dataType.toUpperCase().contains (GlobalConstant.DATA_TYPE_VIRTUAL)) {
+changeUserPreferredLanguage(GlobalConstant.ENGLISH_PREFFERED_LANG_CODE)
+  }
+    }
+ }
+else if(userLanguage.equalsIgnoreCase(GlobalConstant.SPANISH)){
+baseUtil.deviceProp.setPreferredLanguage(GlobalConstant.SPANISH);
+if (!dataType.toUpperCase().contains (GlobalConstant.DATA_TYPE_VIRTUAL)) {
+changeUserPreferredLanguage(GlobalConstant.SPANISH_PREFFERED_LANG_CODE);
+     }
+}
+}
+}
+}
