6d35d5bcbfb953640a9b578c83e1a8c007bfa1c7
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-07-05 21:49:20 -0700

    Update RotationMap.java

--- RotationMap.java
+++ RotationMap.java
@@ -11,24 +11,24 @@ public class RotationMap {
 
 @When("Customer authenticates the API key for location")
   public void customerAuthenticatesTheAPIKeyForLocation(){
-    String yourAPIKey = System.getProperty("apiKey");
-      ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
+    String customerApiKey = System.getProperty("apiKey");
+      ArcGISRuntimeEnvironment.setApiKey(customerApiKey);
   }
 
 @Then("User verfies text on ARCGIS Map informing customer how to rotate map")
-  public void userPointsTheCompassNorthOnARCGIS(){
+  public void userVerifiesTextOnARCGISMapInformingCustomerHowToRotateMap(){
 
     // creating a new map on ARCGIS Map
-    mapView = new MapView();
-      mapView.setMap(map);
+    arcgisMap = new arcgisMap();
+      arcgisMap.setMap(map);
 
     // creating a compass for the map
-     int compass = new Compass(mapView);
+     int compass = new Compass(viewMap);
 
-    // Creating and confirming the label
-      int instructionsLabel = new Label("Press the A and D keys to rotate the map.");
-      instructionsLabel.setTextFill(Color.RED);
-      int rotationLabel = new Label("Current rotation: ");
-      rotationLabel.setTextFill(Color.RED);
+    // Creating and confirming the text on the Map
+      int textLabel = new Label("Press the A and D keys to rotate the map.");
+      textLabel.setTextFill(Color.RED);
+      int rotationinformation = new Label("Current rotation: ");
+      rotationinformation.setTextFill(Color.RED);
   }
 }
